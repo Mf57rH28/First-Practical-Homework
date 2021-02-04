ab49a3c41ea334904ffe69aab89f37ad6c9d40ce
ASUSM <ASUSM@192.168.1.13>
2021-02-04 12:15:32 +0300

    Frist commit

---
+++ FirstPracticalHomework/src/FirstPracticalHomework.java
@@ -0,0 +1,14 @@
+//This program is written by Mufarreh Salem Alshihrey 
+//Date: Feb 4,2021 
+//Course Name:  212CSS-4 Object Oriented Programming
+
+public class FirstPracticalHomework {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Mufarreh Salem");
+
+	}
+
+}
